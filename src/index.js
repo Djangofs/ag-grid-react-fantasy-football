4135b02cd3594691827547f51183fb8f6bda141f
Django Shelton <djangoshelton@gmail.com>
2017-11-21 20:09:44 +0000

    add simple styles

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {render} from "react-dom";
 // pull in the ag-grid styles we're interested in
 import "ag-grid-root/dist/styles/ag-grid.css";
 import "ag-grid-root/dist/styles/theme-fresh.css";
+import "./styles.css"
 
 // only necessary if you're using ag-Grid-Enterprise features
 // import "ag-grid-enterprise";
