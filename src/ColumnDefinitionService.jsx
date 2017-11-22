4b67993b03a7bbd41e0d22c23a96bf24c5a04379
Django Shelton <djangoshelton@gmail.com>
2017-11-22 15:53:39 +0000

    add class rules for exact number

--- src/ColumnDefinitionService.jsx
+++ src/ColumnDefinitionService.jsx
@@ -2,17 +2,17 @@ import TeamCellRenderer from "./TeamCellRenderer";
 
 const createColumnDefs = () => {
   const formClassRules = {
-      'rag-green': 'x > 6',
+      'rag-green': 'x => 6',
       'rag-amber': 'x >= 4 && x < 6',
       'rag-red': 'x < 4'
   }
   const ictIndexClassRules = {
-      'rag-green': 'x > 90',
+      'rag-green': 'x => 90',
       'rag-amber': 'x >= 60 && x < 90',
       'rag-red': 'x < 60'
   }
   const selectedPercentClassRules = {
-      'rag-green': 'x > 20',
+      'rag-green': 'x => 20',
       'rag-amber': 'x >= 10 && x < 20',
       'rag-red': 'x < 10'
   }
@@ -42,6 +42,7 @@ const createColumnDefs = () => {
               {headerName: "Form", field: "form", filter: "number", cellClassRules: formClassRules},
               {headerName: "ICT Index", field: "ict_index", filter: "number", cellClassRules: ictIndexClassRules},
               {headerName: "Selected %", field: "selected_by_percent", filter: "number", cellClassRules: selectedPercentClassRules},
+              {headerName: "Cost", field: "now_cost", filter: "number"},
               {headerName: "Total Points", field: "total_points", filter: "number"},
           ]
       }
